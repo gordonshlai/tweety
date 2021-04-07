aeea0a496bd6439f01cad501a24a1743c9441e82
Gordon Lai <gordonshlai@yahoo.com.hk>
2021-04-07 14:30:23 +0100

    Make the Timeline Dynamic

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,15 +1,12 @@
 <div class='flex p-4 border-b border-b-gray-400'>
     <div class='mr-2 flex-shrink-0'>
-        <img src="https://i.pravatar.cc/50" alt="" class='rounded-full mr-2'>
+        <img src="{{ $tweet->user->avatar }}" alt="" class='rounded-full mr-2'>
     </div>
 
     <div>
-        <h5 class='font-bold mb-4'>John Doe</h5>
+        <h5 class='font-bold mb-4'>{{ $tweet->user->name }}</h5>
         <p class='text-sm'>
-            This is a paragraph. It is very long and it contains a lot of information. But the
-            information are not very useful, by the time you have read til here, you will realise this
-            is a uselessparagraph that just to take up some space, and you won't have any idea what is
-            this for. Don'tworry, everyone feels the same way as you do.
+            {{ $tweet->body }}
         </p>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::post('/tweets', 'App\Http\Controllers\TweetsController@store');
 
-require __DIR__.'/auth.php';
+Route::get('/dashboard', function () {
+    return view('dashboard', ['tweets' => auth()->user()->timeline()]);
+})->middleware(['auth'])->name('dashboard');
+require __DIR__ . '/auth.php';
