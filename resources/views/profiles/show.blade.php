6fa9912632bf3d40841d47f79dd6b226967a0e55
Gordon Lai <gordonshlai@yahoo.com.hk>
2021-04-15 14:44:15 +0100

    Build the Follow Form.

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,15 +1,23 @@
 <x-app-layout>
     <header class='mb-6 relative'>
-        <img src="/images/corsa.jpeg" alt="" class='mb-2 rounded-3xl' style='height: 233px; width: 100%'>
-        <div class='flex justify-between items-center mb-4'>
+        <div class='relative'>
+            <img src="/images/corsa.jpeg" alt="" class='mb-2 rounded-3xl' style='height: 233px; width: 100%'>
+            <img src="{{ $user->avatar }}" alt=""
+                class='rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2' width='150'
+                style='left: 50%'>
+        </div>
+        <div class='flex justify-between items-center mb-6'>
             <div>
                 <h2 class='font-bold text-2xl'>{{ $user->name }}</h2>
                 <p class='text-sm'>Joined {{ $user->created_at->diffForHumans() }}</p>
             </div>
-            <div>
+            <div class='flex'>
                 <a href="" class='rounded-full border boder-gray-300 py-2 px-4 text-black text-xs mr-2'>Edit
-                    Profile</a>
-                <a href="" class='bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs'>Follow Me</a>
+                    Profile
+                </a>
+
+                <x-follow-button :user='$user'></x-follow-button>
+
             </div>
         </div>
 
@@ -17,10 +25,8 @@
             hard coded one.
             Here is the description of the user, hard coded one. Here is the description of the user, hard coded
             one. Here is the description of the user, hard coded one. Here is the description of the user, hard
-            coded one. Here is the description of the user, hard coded one.</p>
-
-        <img src="{{ $user->avatar }}" alt="" class='rounded-full mr-2 absolute'
-            style='width: 150px; left:calc(50% - 75px); top: 138px'>
+            coded one. Here is the description of the user, hard coded one.
+        </p>
 
     </header>
     @include('_timeline', [
